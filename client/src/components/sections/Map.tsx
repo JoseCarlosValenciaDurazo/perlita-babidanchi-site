271e80914160f69bd97a9f3de4a12e8a8779229f
JCarlosValencia <39584880-JCarlosValencia@users.noreply.replit.com>
2025-02-13 00:20:53 +0000

    Agent query: I've replaced the map with the Google Maps embed you provided. Is the map displaying correctly in the investment section?
    Replace interactive map with Google Maps iframe.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/5e33f91c-177f-4767-81a7-db37241aeb63/35787f75-4049-4f18-b952-651b8e2030f2.jpg

--- client/src/components/sections/Map.tsx
+++ client/src/components/sections/Map.tsx
@@ -1,26 +1,15 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-
 export default function Map() {
   return (
     <div className="h-[400px] w-full rounded-lg overflow-hidden relative" style={{ zIndex: 0 }}>
-      <MapContainer
-        center={[30.3666667, -109.2]}
-        zoom={10}
-        style={{ height: '100%', width: '100%' }}
-        className="z-[1]"
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={[30.3666667, -109.2]}>
-          <Popup>
-            Mina Babidanchi<br />
-            Km 10, NE, de Aribabi, Son.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <iframe 
+        src="https://www.google.com/maps/d/u/0/embed?mid=1fFbg1eX9zGB4f1DPDanI3EhccKrOEfc&ehbc=2E312F" 
+        width="100%" 
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      />
     </div>
   );
 }
